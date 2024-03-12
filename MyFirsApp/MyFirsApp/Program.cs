6e88402f56134dc46fc5d68fe6a1b46c3f3884cb
s27943 <s27943@pjwstk.edu.pl>
2024-03-12 17:02:56 +0100

    Print average on the console

--- MyFirsApp/MyFirsApp/Program.cs
+++ MyFirsApp/MyFirsApp/Program.cs
@@ -8,11 +8,16 @@ Console.WriteLine("Modyfikacja 3");
 static double GetAverage(int[] numbers)
 {
     double sum = 0;
-    
+
     for (int i = 0; i < numbers.Length; i++)
     {
-        sum += numebrs[i];
+        sum += numbers[i];
     }
 
     return sum / numbers.Length;
 }
+
+int[] numbers = { 1, 2, 3, 4 };
+
+    double average = GetAverage(numbers);
+    Console.WriteLine(average);
